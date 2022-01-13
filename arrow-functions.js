bae6afdf77b2241e4c7c44d479b125e562d38cc3
JayKearney <indi.mh@gmail.com>
2022-01-13 21:19:03 +0000

    add arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,10 +4,23 @@
  */
 
 // Vanilla JavaScript Function
+//function addTwoNumbers(a,b) {
+    // Code block
+    //return a + b;
+}
+//let sum = addTwoNumbers(3,5);
+//console.log(sum);
 
 // Arrow Function With Parameters
+  const addtwonumbers = (a,b) => {
+    // Code block
+   return a + b;
+   }
+  let numbers = addtwonumbers(3,5);
+  console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => (a + b);
 
 // Implicit Returns
 
